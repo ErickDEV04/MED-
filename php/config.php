eb6a6b1ee70f115995d58fdf5cc1e733ad042c68
Erick Silva <erick04develope@gmail.com>
2025-05-13 12:39:09 -0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,1 +1,15 @@
+<?php
+$host = 'localhost';
+$db = 'medplus';
+$user = 'root';
+$pass = '';
 
+try {
+    $conn = new mysqli($host, $user, $pass, $db);
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    die("Erro: " . $e->getMessage());
+}
+?>
